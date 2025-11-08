184ff4a9e7c819a4ee8628ffca1ab7bf60373119
Arpit Thakur <thakurarpit1003@gmail.com>
2025-11-08 16:44:54 -0500

    Build default landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 import Dashboard from './pages/Dashboard/Dashboard';
 import SignUpPage from './pages/SignUp/SignUpPage';
 import LoginPage from './pages/Login/LoginPage';
@@ -7,6 +8,7 @@ import LoginPage from './pages/Login/LoginPage';
 const App = () => (
   <BrowserRouter>
     <Routes>
+      <Route path="/" element={<LandingPage />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/login" element={<LoginPage />} />
