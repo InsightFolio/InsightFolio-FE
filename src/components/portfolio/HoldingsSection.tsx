c8f0c14a48f12ed0af9882554d8531a23f1cd1d8
Arpit Thakur <thakurarpit1003@gmail.com>
2025-11-23 14:24:34 -0500

    fixed holding logic

--- src/components/portfolio/HoldingsSection.tsx
+++ src/components/portfolio/HoldingsSection.tsx
@@ -24,6 +24,7 @@ const HoldingsSection: React.FC<HoldingsSectionProps> = ({ holdings, onSelect })
       <div className="stock-list__row stock-list__row--header portfolio__row">
         <span role="columnheader">Symbol</span>
         <span role="columnheader">Company</span>
+        <span role="columnheader">Shares</span>
         <span role="columnheader">Amount Held</span>
         <span role="columnheader">% Growth</span>
       </div>
@@ -40,6 +41,9 @@ const HoldingsSection: React.FC<HoldingsSectionProps> = ({ holdings, onSelect })
           </span>
           <span role="cell" className="stock-list__cell stock-list__cell--company">
             {holding.company}
+          </span>
+          <span role="cell" className="stock-list__cell stock-list__cell--shares">
+            {holding.shares}
           </span>
           <span role="cell" className="stock-list__cell stock-list__cell--value">
             ${holding.value.toLocaleString()}
